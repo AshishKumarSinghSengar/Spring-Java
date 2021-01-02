c24b6e1b2ca157ba0aece473bf948008289d3e7a
AshishKumarSinghSengar <65999078+AshishKumarSinghSengar@users.noreply.github.com>
2021-01-02 16:31:39 +0530

    API model Part-1
    --Annotations Added
    --Packages added

---
+++ src/main/java/com/example/demo/API/PersonController.java
@@ -0,0 +1,29 @@
+package com.example.demo.API;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("pi/vl/person")
+@RestController
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService){
+        this.personService = personService;
+    }
+    @PostMapping
+    public void addPerson(@RequestBody Person person){
+        personService.addPerson(person);
+    }
+    
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+}
